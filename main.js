2b3202428a223985a20fd191ee1978e378d7b63b
saidaz24-meet <139681209+saidaz24-meet@users.noreply.github.com>
2023-07-17 16:11:17 +0300

    rur

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 function  changeBackgroundColor(color){
-	document.body.style.backgroundColor = color;
+	document.body.style.backgroundColor = color; 
+}
+
+function randombg(){
+	const colors = ["red","green","brown","white","black","orange","yellow","purple"];
+	document.body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
 }
